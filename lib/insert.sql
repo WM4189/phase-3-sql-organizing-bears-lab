9b71c9edc6660ff4f3575434b45a35d5353e834f
William Mosto <will.mosto@gmail.com>
2021-09-23 12:58:50 -0400

    commit tree c4ff8a016174fb2dd081ef6d93ce15f839cf92ec

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 22, 'M', 'Green', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 22, 'M', 'Green', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 22, 'M', 'Green', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 22, 'M', 'Green', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 22, 'M', 'Green', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 22, 'M', 'Green', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 22, 'M', 'Green', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 22, 'M', 'Green', 'Wild', 1);
+
+
+
